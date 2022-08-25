6b2e4747149a5955cf96fe1006697ba3bb85c76f
alanwu <alanwu@vesync.com.cn>
2022-08-25 13:57:11 +0800

    kafka发送消息为自定义对象&将消息转为自定义对象

--- src/main/java/com/whs/kafkademo/controller/ConsumerListener.java
+++ src/main/java/com/whs/kafkademo/controller/ConsumerListener.java
@@ -1,13 +1,25 @@
 package com.whs.kafkademo.controller;
 
+import com.google.gson.Gson;
+import com.whs.kafkademo.entity.User;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConsumerListener {
+
+    private static final String USER_INFO_TOPIC = "userInfo";
+
     @KafkaListener(topics = "test")
     public void listener(ConsumerRecord<?, ?> record) {
         System.out.println(String.format("topic:%s, offset:%d, value:%s ", record.topic(), record.offset(), record.value()));
+    }
+
+    @KafkaListener(topics = USER_INFO_TOPIC)
+    public void listenerUserInfo(ConsumerRecord<?, ?> record) {
+        Gson gson = new Gson();
+        User user = gson.fromJson(record.value().toString(), User.class);
+        System.out.println(String.format("user info, age=%d, name=%s", user.getAge(), user.getName()));
     }
 }
--- src/main/java/com/whs/kafkademo/controller/ProducerController.java
+++ src/main/java/com/whs/kafkademo/controller/ProducerController.java
@@ -1,5 +1,7 @@
 package com.whs.kafkademo.controller;
 
+import com.google.gson.Gson;
+import com.whs.kafkademo.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/kafka")
 public class ProducerController {
+
     int msgNo = 0;
 
     /**
@@ -20,10 +23,18 @@ public class ProducerController {
 
     /**
      * 发送消息
+     * localhost:8012/kafka/send?msg=whs
      * @param msg
      */
     @GetMapping("/send")
     public void send(@RequestParam String msg) {
         kafkaTemplate.send("test", msg + msgNo++);
+    }
+
+    @GetMapping("/sendUser")
+    public void sendUser(@RequestParam String name) {
+        Gson gson = new Gson();
+        User user = new User(1, name);
+        kafkaTemplate.send("userInfo", gson.toJson(user));
     }
 }
